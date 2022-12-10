885828edcba966465743518b168b064fbdea4803
Sofia_Gutierrez <sofiagutierrez1845@gmail.com>
2022-12-10 17:27:14 -0300

    estilizando la arrow de proyectos y cambiando el background general

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -16,7 +16,19 @@ const Cover = () => {
                     <li><a target="_blank" rel="noreferrer" href="https://github.com/Sofia-Gutierrez"><i className="fa-brands fa-github"></i></a></li>
                     <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/sofia-g-gutierrez/"><i className="fa-brands fa-linkedin"></i></a></li>
                 </ul>
-                <Link to="works" spy={true} smooth={true} offset={50} duration={500}><i className="fa-solid fa-arrow-down"></i></Link>    
+                <div className="container">
+                    <div className="content-arrow">
+                        <p className="arrow-text">Proyectos</p>
+                        <Link to="works" spy={true} smooth={true} offset={50} duration={500}>
+                        <svg id="more-arrows">
+                        <polygon className="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
+                        <polygon className="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
+                        <polygon className="arrow-bottom" points="37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1 "/>
+                        </svg>
+                        </Link>    
+                    </div>
+                </div>
+                
             </div>
         )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,20 +6,20 @@ const Navbar = () => {
 
     useAos();
 
-        return(
-            <nav className="nav" data-aos="fade-down">  
-                <input type="checkbox" id="toggler" />
-                <label for="toggler"><i className="fa-solid fa-bars"></i></label>
-                <div className="nav-content">
-                    <ul className="nav-links">
-                        <li className="nav-li"><Link className="link-i" to="home" spy={true} smooth={true} offset={50} duration={500}><i className="fa-solid fa-house"></i></Link></li>
-                        <li className="nav-li"><Link to="works" spy={true} smooth={true} offset={50} duration={500} className="link">Proyectos </Link></li>
-                        <li className="nav-li"><Link className="link" to="about" spy={true} smooth={true} offset={-50} duration={500}>Sobre mi </Link></li>
-                        <li className="nav-li"><Link className="link" to="about" spy={true} smooth={true} offset={50} duration={500}>Contacto </Link></li>
-                        <li className="nav-li"><Link className="link" to="technologies" spy={true} smooth={true} offset={50} duration={500}>Tecnologias</Link></li>
-                    </ul>
-                </div>
-            </nav>
+    return(
+        <nav className="nav" data-aos="fade-down">  
+            <input type="checkbox" id="toggler" />
+            <label for="toggler"><i className="fa-solid fa-bars"></i></label>
+            <div className="nav-content">
+                <ul className="nav-links">
+                    <li className="nav-li"><Link className="link" to="home" spy={true} smooth={true} offset={-50} duration={500}>Inicio</Link></li>
+                    <li className="nav-li"><Link to="works" spy={true} smooth={true} offset={50} duration={500} className="link">Proyectos </Link></li>
+                    <li className="nav-li"><Link className="link" to="about" spy={true} smooth={true} offset={-100} duration={500}>Sobre mi </Link></li>
+                    <li className="nav-li"><Link className="link" to="about" spy={true} smooth={true} offset={50} duration={500}>Contacto </Link></li>
+                    <li className="nav-li"><Link className="link" to="technologies" spy={true} smooth={true} offset={50} duration={500}>Tecnologias</Link></li>
+                </ul>
+            </div>
+        </nav>
         )
 }
 
